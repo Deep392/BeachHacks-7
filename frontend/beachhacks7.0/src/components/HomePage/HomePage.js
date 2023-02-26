c0ee65d353dd38a1d65a12b2f8fa7fa089bdc8e5
Deepmeghani <deepmeghani1@gmail.com>
2023-02-25 20:12:22 -0800

    Homepage final

--- frontend/beachhacks7.0/src/components/HomePage/HomePage.js
+++ frontend/beachhacks7.0/src/components/HomePage/HomePage.js
@@ -1,5 +1,79 @@
+import {useEffect, useState} from 'react';
+import './HomePage.css'
+import Sidenav from "../Sidenav/Sidenav"
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { auth, db } from "../../firebase-config";
+import {Button} from 'grommet'
+import Comments from '../Comments/Comments';
+import "./HomePage.css";
+/*
+    Here we have the main Login, if the User is not Logged in, they will see this Header
+ */
 const HomePage = () => {
+  const [postLists, setPostList] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [open, setOpen] = useState(false);
 
-}
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(postLists);
+      console.log(comments);
+    };
+
+    getPosts();
+  }, []);
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+  };
+    
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1> {post.title}</h1>
+              </div>
+              <div className="deletePost">
+                {(
+                  <button
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    {" "}
+                    &#128465;
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="postTextContainer"> {post.postText} </div>
+            <div className='buttons'>
+              <button className='button'>Like</button>
+              <button className='button' onClick={handleOpen}>
+                <Comments
+                  postId={post.id}
+                  className="faqBox"
+                ></Comments>
+              </button>
+            </div>
+            {/* <h3>@{post.author.name}</h3> */}
+          </div>
+        );
+      })}
+    </div>
+  );
+
+};
 
 export default HomePage;

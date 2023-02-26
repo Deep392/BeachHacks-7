6880fbebab55c44d320a2ad6bc8da46194449e02
HetMakadia <65148978+HetMakadia@users.noreply.github.com>
2023-02-25 17:08:49 -0800

    Modified App.js that contains the routing after the authentication of user. Also added the styling for App.js. Created simple navbar using daisyUI and tailwind components.

--- frontend/beachhacks7.0/src/App.js
+++ frontend/beachhacks7.0/src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+// Importing Components
+import Sidenav from "./components/Sidenav/Sidenav";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./components/HomePage/HomePage";
+import AddPost from "./components/AddPost/AddPost"
+import Messages from "./components/Messages/Messages";
+import Advising from "./components/Advising/Advising";
+import ContactUs from "./components/ContactUs/ContactUs"
+import Login from "./components/LogIn/Login";
+import Signup from "./components/Signup/Signup";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+    return (
+        <Router>
+            <nav className="nav ">
+                {!isAuth ? (
+                    <div className="grid grid-cols-1 gap-10  place-content-center justify-items-center">
+                        <h1 className="logo-text main-header-text">stuXpert</h1>
+
+                        <div className="flex flex-col lg:flex-row">
+                            <div className="grid flex-grow card place-items-center"><Signup/></div>
+                            <div className="divider lg:divider-horizontal"></div>
+                            <div className="grid flex-grow card place-items-center"><Login setIsAuth={setIsAuth}/></div>
+                        </div>
+                    </div>
+                ) : (
+                    <>
+                        <div className="grid grid-rows-12">
+                            <Router>
+                                <div className='row-start-1 row-end-2 sticky top-0'>
+                                    <Navbar/>
+                                </div>
+
+                                <div className="row-start-2 row-end-11">
+                                    <div className="fixed">
+                                        <Sidenav/>
+                                    </div>
+                                    <div className="flex items-center justify-center">
+                                        <Routes>
+                                            <Route exact path="/HomePage" component={HomePage}/>
+                                            <Route exact path="/AddPost" component={AddPost}/>
+                                            <Route exact path="/Messages" component={Messages}/>
+                                            <Route exact path="/Advising" component={Advising}/>
+                                            <Route exact path="/ContactUs" component={ContactUs}/>
+                                        </Routes>
+                                    </div>
+                                </div>
+
+                                <div className='row-start-11 row-end-12 '>
+                                    <Footer/>
+                                </div>
+                            </Router>
+                        </div>
+                    </>
+                )}
+            </nav>
+        </Router>
+
+    );
 }
 
 export default App;

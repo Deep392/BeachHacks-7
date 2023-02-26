48cfd5a82e3fc039f1e5e8f4aa4e58309d3f8f2b
HetMakadia <65148978+HetMakadia@users.noreply.github.com>
2023-02-25 21:40:19 -0800

    Improved CSS for Sidenav

--- frontend/beachhacks7.0/src/components/Sidenav/Sidenav.js
+++ frontend/beachhacks7.0/src/components/Sidenav/Sidenav.js
@@ -8,24 +8,15 @@ import Addpost from "../AddPost/Addpost";
 
 function Sidenav(){
     return (
-
-        <div className="drawer drawer-mobile bg-neutral">
-            <div className="drawer-content flex flex-col items-center justify-center">
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-            </div>
+        <div className="drawer bg-neutral">
             <div className="drawer-side ">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-40 text-base-content">
-
                     <li className="pb-3"><a><Link to="/HomePage">Home</Link></a></li>
                     <li className="pb-3"><a><Link to="/AddPost">Add a Post</Link></a></li>
                     <li className="pb-3"><a><Link to="/Messages">Messages</Link></a></li>
                     <li className="pb-3"><a><Link to="/Advising">Advising</Link></a></li>
                     <li className="pb-3"><a><Link to="/ContactUs">Contact Us</Link></a></li>
-
-                    <div className="drawer-content flex flex-col items-center justify-center">
-                        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-                    </div>
                 </ul>
 
             </div>

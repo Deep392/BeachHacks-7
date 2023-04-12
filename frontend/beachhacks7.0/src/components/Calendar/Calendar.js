b9fbd7cd159585f7ba59475d730daafb87537036
omshah11 <61638057+omshah11@users.noreply.github.com>
2023-04-11 22:38:41 -0700

    more changes

--- frontend/beachhacks7.0/src/components/Calendar/Calendar.js
+++ frontend/beachhacks7.0/src/components/Calendar/Calendar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 // import "./App.css";
-import { gapi } from "gapi-script";
+// import { gapi } from "gapi-script";
 // import Event from "./components/Event.js";
 import {Link} from "react-router-dom";
  
@@ -13,7 +13,7 @@ const Calendar = () => {
   const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
   const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
   
-  let gapi = window.gapi;
+  var gapi = window.gapi;
 
   const handleClick = () => {
     gapi.load('client:auth2', () => {
@@ -29,67 +29,67 @@ const Calendar = () => {
       gapi.client.load('calendar', 'v3', () => console.log('bam!'))
 
       gapi.auth2.getAuthInstance().signIn()
-      .then(() => {
+      // .then(() => {
         
-        var event = {
-          'summary': 'Awesome Event!',
-          'location': '800 Howard St., San Francisco, CA 94103',
-          'description': 'Really great refreshments',
-          'start': {
-            'dateTime': '2020-06-28T09:00:00-07:00',
-            'timeZone': 'America/Los_Angeles'
-          },
-          'end': {
-            'dateTime': '2020-06-28T17:00:00-07:00',
-            'timeZone': 'America/Los_Angeles'
-          },
-          'recurrence': [
-            'RRULE:FREQ=DAILY;COUNT=2'
-          ],
-          'attendees': [
-            {'email': 'lpage@example.com'},
-            {'email': 'sbrin@example.com'}
-          ],
-          'reminders': {
-            'useDefault': false,
-            'overrides': [
-              {'method': 'email', 'minutes': 24 * 60},
-              {'method': 'popup', 'minutes': 10}
-            ]
-          }
-        }
+      //   var event = {
+      //     'summary': 'Awesome Event!',
+      //     'location': '800 Howard St., San Francisco, CA 94103',
+      //     'description': 'Really great refreshments',
+      //     'start': {
+      //       'dateTime': '2020-06-28T09:00:00-07:00',
+      //       'timeZone': 'America/Los_Angeles'
+      //     },
+      //     'end': {
+      //       'dateTime': '2020-06-28T17:00:00-07:00',
+      //       'timeZone': 'America/Los_Angeles'
+      //     },
+      //     'recurrence': [
+      //       'RRULE:FREQ=DAILY;COUNT=2'
+      //     ],
+      //     'attendees': [
+      //       {'email': 'lpage@example.com'},
+      //       {'email': 'sbrin@example.com'}
+      //     ],
+      //     'reminders': {
+      //       'useDefault': false,
+      //       'overrides': [
+      //         {'method': 'email', 'minutes': 24 * 60},
+      //         {'method': 'popup', 'minutes': 10}
+      //       ]
+      //     }
+      //   }
 
-        var request = gapi.client.calendar.events.insert({
-          'calendarId': 'primary',
-          'resource': event,
-        })
+      //   var request = gapi.client.calendar.events.insert({
+      //     'calendarId': 'primary',
+      //     'resource': event,
+      //   })
 
-        request.execute(event => {
-          console.log(event)
-          window.open(event.htmlLink)
-        })
+      //   request.execute(event => {
+      //     console.log(event)
+      //     window.open(event.htmlLink)
+      //   })
         
 
-        /*
-            Uncomment the following block to get events
-        */
-        /*
-        // get events
-        gapi.client.calendar.events.list({
-          'calendarId': 'primary',
-          'timeMin': (new Date()).toISOString(),
-          'showDeleted': false,
-          'singleEvents': true,
-          'maxResults': 10,
-          'orderBy': 'startTime'
-        }).then(response => {
-          const events = response.result.items
-          console.log('EVENTS: ', events)
-        })
-        */
+      //   /*
+      //       Uncomment the following block to get events
+      //   */
+      //   /*
+      //   // get events
+      //   gapi.client.calendar.events.list({
+      //     'calendarId': 'primary',
+      //     'timeMin': (new Date()).toISOString(),
+      //     'showDeleted': false,
+      //     'singleEvents': true,
+      //     'maxResults': 10,
+      //     'orderBy': 'startTime'
+      //   }).then(response => {
+      //     const events = response.result.items
+      //     console.log('EVENTS: ', events)
+      //   })
+      //   */
     
 
-      })
+      // })
     })
   }
  
